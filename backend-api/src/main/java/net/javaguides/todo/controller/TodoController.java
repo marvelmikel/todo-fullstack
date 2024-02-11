f0d36ea2216babfdc268c2f4175edc263c11228d
marvelmikel <marvelmikel1997@gmail.com>
2024-02-11 02:04:01 +0000

    delete todo endpoint done

--- backend-api/src/main/java/net/javaguides/todo/controller/TodoController.java
+++ backend-api/src/main/java/net/javaguides/todo/controller/TodoController.java
@@ -48,4 +48,11 @@ public class TodoController {
        return ResponseEntity.ok(updatedTodo);
     }
 
+    // Build DELETE TODO REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteTodo(@PathVariable("id") Long todoId){
+        todoService.deleteTodo(todoId);
+        return ResponseEntity.ok("Todo deleted successfully!.");
+    }
+
 }
