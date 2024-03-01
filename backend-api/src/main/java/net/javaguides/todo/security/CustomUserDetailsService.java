e38174c3f1ade7d5bf8f1d58ae14266466926d5b
marvelmikel <marvelmikel1997@gmail.com>
2024-03-01 00:56:38 +0000

    fixes

--- backend-api/src/main/java/net/javaguides/todo/security/CustomUserDetailsService.java
+++ backend-api/src/main/java/net/javaguides/todo/security/CustomUserDetailsService.java
@@ -17,16 +17,16 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
 
         User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
-                .orElseThrow(() -> new UsernameNotFoundException("User not exists by Username or Email"  + usernameOrEmail));
+                .orElseThrow(() -> new UsernameNotFoundException("User not exists by Username or Email"));
 
         Set<GrantedAuthority> authorities = user.getRoles().stream()
-                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .map((role) -> new SimpleGrantedAuthority(role.getName()))
                 .collect(Collectors.toSet());
 
         return new org.springframework.security.core.userdetails.User(
