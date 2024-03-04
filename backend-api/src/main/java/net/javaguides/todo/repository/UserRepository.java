67de7f2b09d609aadd3e10d3e7c480af028ef4fb
marvelmikel <marvelmikel1997@gmail.com>
2024-03-04 07:18:31 +0000

    fixes

--- backend-api/src/main/java/net/javaguides/todo/repository/UserRepository.java
+++ backend-api/src/main/java/net/javaguides/todo/repository/UserRepository.java
@@ -3,14 +3,13 @@ package net.javaguides.todo.repository;
 import net.javaguides.todo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.StyledEditorKit;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findUsername(String username);
+    Optional<User> findByUsername(String username);
 
-    Boolean existByEmail(String email);
+    Boolean existsByEmail(String email);
 
     Optional<User> findByUsernameOrEmail(String username, String email);
 }
--- backend-api/src/main/java/net/javaguides/todo/security/CustomUserDetailsService.java
+++ backend-api/src/main/java/net/javaguides/todo/security/CustomUserDetailsService.java
@@ -17,16 +17,16 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
 
         User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
-                .orElseThrow(() -> new UsernameNotFoundException("User not exists by Username or Email"));
+                .orElseThrow(() -> new UsernameNotFoundException("User not exists by Username or Email"  + usernameOrEmail));
 
         Set<GrantedAuthority> authorities = user.getRoles().stream()
-                .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
                 .collect(Collectors.toSet());
 
         return new org.springframework.security.core.userdetails.User(
