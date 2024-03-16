a7695b66b54eb6860039bfbb26122276c0fd2c8a
marvelmikel <marvelmikel1997@gmail.com>
2024-03-16 02:04:49 +0000

    error exception handler done

---
+++ backend-api/src/main/java/net/javaguides/todo/exception/ErrorDetails.java
@@ -0,0 +1,20 @@
+package net.javaguides.todo.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetails {
+
+    private LocalDateTime timeSamp;
+    private String message;
+    private String details;
+
+}
