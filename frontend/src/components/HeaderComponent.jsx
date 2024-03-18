6edf3e4580e1a9873af55460ca219928c0419992
marvelmikel <marvelmikel1997@gmail.com>
2024-03-18 06:23:46 +0000

    check if user is logged in or logout

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { isUserLoggedIn } from '../services/AuthService'
+
+const isAuth = isUserLoggedIn();
 
 const HeaderComponent = () => {
   return (
@@ -10,27 +13,39 @@ const HeaderComponent = () => {
               <a className="navbar-brand" href="http://localhost:3000">Todo Management System</a>
               </div>
               <div className='collapse navbar-collapse'>
-                <ul className='navbar-nav'>
+                
+            
+              <ul className='navbar-nav'>
+
+                {
+                  isAuth && 
                   <li className='nav=item'>
                     <NavLink to='/todos' className='nav-link'>
                     Todos
                     </NavLink>
-                  </li>
-                </ul>
+                    </li>
+                }
+              </ul>
               </div>
-              <ul className='navbar-nav'>
-                  <li className='nav=item'>
-                    <NavLink to='/register' className='nav-link'>
-                    Register
-                    </NavLink>
-                  </li>
-                </ul>
+
                 <ul className='navbar-nav'>
-                  <li className='nav=item'>
-                    <NavLink to='/login' className='nav-link'>
-                   Login
-                    </NavLink>
-                  </li>
+                  {
+                    !isAuth &&
+                    <li className='nav=item'>
+                      <NavLink to='/register' className='nav-link'>
+                      Register
+                      </NavLink>
+                    </li>
+                  }
+
+                  {
+                    !isAuth &&
+                    <li className='nav=item'>
+                      <NavLink to='/login' className='nav-link'>
+                      Login
+                      </NavLink>
+                    </li>
+                  }
                 </ul>
             </nav>
         </header>
